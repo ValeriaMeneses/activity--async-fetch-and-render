643ff99ce8d01618709d7e39c32a02d5b8750c4a
valeria <valeriairaismeneses@gmail.com>
2017-10-18 09:39:32 -0500

    Lista de Usuarios

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 const request = superagent
+
+var userList = document.querySelector('.users-list')
+
+document.querySelector('.show-list').addEventListener('click', function () {
+  request
+  .get('https://randomuser.me/api/?results=50')
+  .then(function (dates) {
+    const arrayDates = dates.body.results
+
+    arrayDates.forEach(function (elements) {
+      // console.log(elements.nat);
+
+      userList.innerHTML += `
+      <article>
+        <span>${elements.nat}</span>
+        <h6>${elements.email}</h6>
+      </article>`
+    })
+  })
+
+})
